14bf6e2476d38abe8533b3bee6c15f6fcace4484
Snowspells <wintermicheller@gmail.com>
2026-02-06 17:48:00 -0500

    Began work on new command that will be used for connecting a user's Discord UID to their AGID.

---
+++ src/commands/PoT/slashcommand-link.js
@@ -0,0 +1,25 @@
+const { ChatInputCommandInteraction } = require("discord.js");
+const DiscordBot = require("../../client/DiscordBot");
+const ApplicationCommand = require("../../structure/ApplicationCommand");
+
+module.exports = new ApplicationCommand({
+    command: {
+        name: 'link',
+        description: 'Links Discord ID to AGID',
+        type: 1,
+        options: []
+    },
+    options: {
+        cooldown: 5000
+    },
+    /**
+     * 
+     * @param {DiscordBot} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    run: async (client, interaction) => {
+        await interaction.reply({
+            content: '**Test Complete! (no under-the-hood code was run)**'
+        });
+    }
+}).toJSON();
